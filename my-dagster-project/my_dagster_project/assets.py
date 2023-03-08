64cd09c174e50a26c652be035cef8e875e0aabf9
SantiagoMag <santiagogem29@gmail.com>
2023-03-08 12:51:47 -0500

    feat: initial dagster project

---
+++ my-dagster-project/my_dagster_project/assets.py
@@ -0,0 +1,42 @@
+from dagster import asset
+
+
+@asset
+def write_auna_csv():
+
+
+    return True
+
+
+@asset
+def run_sql_bulk():
+
+
+    return True
+
+@asset
+def run_sql_contact():
+
+
+    return True
+
+
+@asset
+def run_sql_cuentas():
+
+
+    return True
+
+
+@asset
+def run_sql_lt():
+
+    return True
+
+
+@asset
+def generate_lt_xlsx():
+    return True
+
+
+
